ed1a071179730fae5622e6aea63c3561c5f984e7
Pavlo Gusar <pgusar@yahoo.com>
2019-11-25 14:47:36 +0200

    Updating interface methods

--- src/DependentQueue/IDependentQueue.ts
+++ src/DependentQueue/IDependentQueue.ts
@@ -1,6 +1,6 @@
 export default interface IDependentQueue<T> {
-  peek(): T | null;
-  poll(): T | null;
+  peek(type?: string): T | null;
+  poll(type?: string): T | null;
   offer(item: T, depend?: T | T[]): boolean | boolean[];
   moveEnd(item: T): void;
   checkQueueEmpty(type?: string): boolean;
